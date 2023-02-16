fe5d9c742c96cd3975a35f8b3f6014cd978cff8f
invissiblecat <invica@list.ru>
2023-02-16 14:16:16 +0300

    basic image upload added

--- backend/src/metadata/metadata.controller.ts
+++ backend/src/metadata/metadata.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { MetadataService } from './metadata.service';
 import { CreateMetadataDto } from './create-metadata.dto';
 import { Metadata } from '../schemas/metadata.schema';
 import { SignatureGuard } from 'src/signature/signature.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { MetadataImageDto } from './upload-image-dto';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+
 @Controller('metadata')
 export class MetadataController {
   constructor(private readonly metadataService: MetadataService) {}
@@ -16,5 +29,32 @@ export class MetadataController {
   @Get()
   async findAll(): Promise<Metadata[]> {
     return this.metadataService.findAll();
+  }
+
+  @Post('upload')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        tokenId: {
+          type: 'string',
+        },
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@Body() body: MetadataImageDto, @UploadedFile('file') file) {
+    const path = './images';
+    console.log(existsSync(path));
+    if (!existsSync(path)) {
+      mkdirSync(path);
+    }
+
+    writeFileSync(path + '/' + file.originalname, file.buffer);
   }
 }
---
+++ backend/src/metadata/upload-image-dto.ts
@@ -0,0 +1,3 @@
+export class MetadataImageDto {
+  readonly tokenId: string;
+}
