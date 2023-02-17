1e3c78b45a6b98d901fecf5d3b07e725c79c8382
invissiblecat <invica@list.ru>
2023-02-17 18:02:37 +0300

    ipfs wip

--- backend/src/metadata/metadata-images.controller.ts
+++ backend/src/metadata/metadata-images.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Get,
   NotFoundException,
-  Param,
   Post,
   Query,
   Res,
@@ -21,7 +20,10 @@ import { MetadataImageDto } from './dto/upload-image-dto';
 import { existsSync, mkdirSync, unlinkSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 import { Response } from 'express';
+
 const Jimp = require('jimp');
+const CryptoJS = require('crypto-js');
+import { create } from 'ipfs-core';
 
 @Controller('metadata-images')
 export class MetadataImagesController {
@@ -112,5 +114,77 @@ export class MetadataImagesController {
 
     response.download(resolvedPath);
     return response;
+  }
+
+  @Post('ipfsUpload')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        tokenId: {
+          type: 'string',
+        },
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file'))
+  async ipfsUpload(@Body() body: MetadataImageDto, @UploadedFile('file') file) {
+    const ipfs = create();
+    const ctstr = this.encrypt(file.buffer);
+    const r = await ipfs.add(ctstr);
+    console.log({ r });
+  }
+
+  @Get('ipfsDownload')
+  async ipfsDownload(@Query('hash') hash: string) {
+    const ipfs = await create();
+    let file;
+    const f = ipfs.get(hash);
+    console.log({ f });
+  }
+
+  encrypt(argument: Buffer) {
+    const key = 'ENCRYPT_KEY'; //todo to env
+    const ct = CryptoJS.AES.encrypt(argument.toString('hex'), key);
+    const ctstr = ct.toString();
+    return Buffer.from(ctstr);
+  }
+
+  decrypt(encrypted: Uint8Array) {
+    const key = 'ENCRYPT_KEY'; //todo to env
+    var str = new TextDecoder('utf-8').decode(encrypted);
+    console.log({ str });
+    const decrypted = CryptoJS.AES.decrypt(str, key);
+    console.log({ decrypted });
+    str = CryptoJS.enc.Utf8.stringify(decrypted);
+    console.log({ str });
+    const wordArray = CryptoJS.enc.Hex.parse(str);
+    const BaText = this.wordToByteArray(wordArray, wordArray.words.length);
+    return BaText;
+  }
+
+  wordToByteArray(word, length) {
+    var ba = [],
+      i,
+      xFF = 0xff;
+    if (length > 0) ba.push(word >>> 24);
+    if (length > 1) ba.push((word >>> 16) & xFF);
+    if (length > 2) ba.push((word >>> 8) & xFF);
+    if (length > 3) ba.push(word & xFF);
+    return ba;
+  }
+
+  toArrayBuffer(buffer) {
+    const arrayBuffer = new ArrayBuffer(buffer.length);
+    const view = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < buffer.length; ++i) {
+      view[i] = buffer[i];
+    }
+    return arrayBuffer;
   }
 }
